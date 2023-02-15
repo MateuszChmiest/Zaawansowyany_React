392112bef675e52448d6ebf568df83009c90896b
Mateusz Chmiest <mateusz_269@icloud.com>
2023-02-15 16:42:01 +0100

    test

--- favnotes/src/App.js
+++ favnotes/src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+  render() {
   return (
     <div className="App">
       <header className="App-header">
@@ -19,11 +20,12 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reactd
         </a>
       </header>
     </div>
   );
+  }
 }
 
 export default App;
